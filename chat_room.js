94f0a8d69359baf508827b398176ba2ec79bc012
adrien <221212@umons.ac.be>
2025-02-16 15:09:39 +0100

    add scroll, and ...

--- chat_room.js
+++ chat_room.js
@@ -1,21 +1,30 @@
 
 const url = "http://127.0.0.1:11434/api/generate";
 
-const persona = "You are a teacher whose goal is to explain each steps and submit small excercices to find the solution of the question that was given by your student. For learning purpose you don't give any response, only tips. After each steps, end with an exercice if possible to check his understanding and then wait a input : the answer of the exercice or 'next' if there is no exercice. If the answer is wrong try to explain the last step differently. Here is the question of your student :"
+const persona = "You are a teacher whose goal is to explain each steps and submit small excercices to find the solution of the question that I'll provide you. For learning purpose you don't give any response, only tips. After each steps, give an exercice if possible to check my understanding and then wait a input : the answer of the exercice if there is one or 'next' if not. If my answer is wrong try to explain the last step differently. Here is my question :"
 let chat = [[0, persona]];
 
 function query() {
 
     let prompt = document.getElementById('fname').value;
+    document.getElementById('fname').value = "";
     chat.push([0, prompt]);
     createChatMessage(0, prompt)
 
-    console.log(chat.map(x => (x[0] ? "IA :":"User : ") + x[1]).join("\n\n"))
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = "..."
+    newDiv.classList.add("container");
+    const currentDiv = document.getElementById("scroll-container");
+    // add the text node to the newly created div
+    currentDiv.appendChild(newDiv);
+    currentDiv.scrollTo(0, currentDiv.scrollHeight);
+
+    console.log(chat.map(x => (x[0] ? " :":"User : ") + x[1]).join("\n\n"))
     fetch(url, {
         method: "POST",
         body: JSON.stringify({
             model: "llama3.2",
-            prompt: chat.map(x => (x[0] ? "IA :":"User : ") + x[1]).join("\n\n"),
+            prompt: chat.map(x => (x[0] ? "Here what you said ":"Here what I said : ") + x[1]).join("\n\n"),
             stream: false,
         }),
         headers: {
@@ -27,35 +36,16 @@ function query() {
         })
         .then(function (data) {
             chat.push([1, data.response]);
-            createChatMessage(1, data.response)
-        })
-        .catch(function (err) {
-            console.log("Fetch Error :-S", err);
-        });
-}
 
-function check_response() {
-    console.log("asking AI...")
-    let prompt = document.getElementById('fname').value;
-    createChatMessage(0, prompt)
+            newDiv.innerHTML = "";
+            sentences = data.response.split("\n")
+            sentences.forEach(s => {
+                const e = document.createElement("p");
+                e.innerHTML = s;
+                newDiv.appendChild(e);
+            });
 
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            model: "llama3.2",
-            prompt: `Is "${prompt}" the answer at the question "${last_question}" ? answer with Yes or No.`,
-            stream: false,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-        },
-    })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.response)
-            createChatMessage(1, data.response)
+            currentDiv.scrollTo(0, currentDiv.scrollHeight);
         })
         .catch(function (err) {
             console.log("Fetch Error :-S", err);
@@ -64,24 +54,19 @@ function check_response() {
 
 function createChatMessage(who, text)
 {
-
-    console.log(text)
-
     const newDiv = document.createElement("div");
 
     sentences = text.split("\n")
     sentences.forEach(s => {
-        newDiv.appendChild(document.createTextNode(s));
-        newDiv.appendChild(document.createElement("br"));
+        const e = document.createElement("p");
+        e.innerHTML = s;
+        newDiv.appendChild(e);
+        // newDiv.appendChild(document.createElement("br"));
     });
     
     newDiv.classList.add("container");
-    if (who) {
+    if (!who)
         newDiv.classList.add("darker");
-        newDiv.classList.add("left");
-    } else {
-        newDiv.classList.add("right");
-    }
     
     const currentDiv = document.getElementById("scroll-container");
     // add the text node to the newly created div
